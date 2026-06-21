a7b82d337762d95f9205e81550871114b55f344e
Dan Greco <git@dangre.co>
2026-06-21 09:06:20 -0400

    fix: remove unused sensor inheritance

--- esphome/components/wmmm/wmmm.h
+++ esphome/components/wmmm/wmmm.h
@@ -1,15 +1,12 @@
 #pragma once
 
-#include "esphome/components/sensor/sensor.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/core/component.h"
 
 namespace esphome {
 namespace wmmm {
 
-class WmmmComponent : public Component,
-                      public uart::UARTDevice,
-                      public sensor::Sensor {
+class WmmmComponent : public Component, public uart::UARTDevice {
 public:
   void setup() override;
   void loop() override;
