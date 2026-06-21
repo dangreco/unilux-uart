7b3434d26b21fb5cf77ecb98fa2be22ae3e4d169
Dan Greco <git@dangre.co>
2026-06-20 21:31:31 -0400

    fix: fix esphome config, log (#8)

--- esphome/components/wmmm/__init__.py
+++ esphome/components/wmmm/__init__.py
@@ -1,9 +1,10 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, uart
 from esphome.const import (
     CONF_ID,
 )
+
+from esphome.components import sensor, uart
 
 CODEOWNERS = ["@dangreco"]
 DEPENDENCIES = ["uart"]
@@ -25,6 +26,6 @@ CONFIG_SCHEMA = (
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config(CONF_ID))
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
